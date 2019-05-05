3452093ed3f1b4a3361891536e9bbf9fb8b2de39
orzechdev <tarasiukdawid@gmail.com>
2019-05-05 23:00:08 +0200

    Add Ingredient add Activity

--- app/src/main/java/com/pizzachefassistant/ui/main/IngredientsViewModel.java
+++ app/src/main/java/com/pizzachefassistant/ui/main/IngredientsViewModel.java
@@ -3,10 +3,15 @@ package com.pizzachefassistant.ui.main;
 import android.app.Application;
 import android.arch.lifecycle.AndroidViewModel;
 import android.arch.lifecycle.LiveData;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+import android.view.View;
 
 import com.pizzachefassistant.App;
 import com.pizzachefassistant.repository.MainRepository;
 import com.pizzachefassistant.repository.model.Ingredient;
+import com.pizzachefassistant.ui.IngredientEditActivity;
 
 import java.util.List;
 
@@ -27,5 +32,14 @@ public class IngredientsViewModel extends AndroidViewModel {
     private void mapLiveDataFromRepo() {
         ingredients = mainRepository.getIngredientList();
         exampleText = mainRepository.getIngredientsExampleText();
+    }
+
+    public void onClickFab(View view) {
+        Log.i("vm", "onClickFab");
+
+        Context context = view.getContext();
+        Intent intent = new Intent(context, IngredientEditActivity.class);
+        intent.putExtra("isAdd", true);
+        context.startActivity(intent);
     }
 }
