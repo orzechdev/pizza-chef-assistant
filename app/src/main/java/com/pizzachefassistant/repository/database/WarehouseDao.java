8f57483c025d54604cc4c77865f98aad94155abe
orzechdev <tarasiukdawid@gmail.com>
2019-05-19 18:50:17 +0200

    CHanges in warehouse table

---
+++ app/src/main/java/com/pizzachefassistant/repository/database/WarehouseDao.java
@@ -0,0 +1,42 @@
+package com.pizzachefassistant.repository.database;
+
+import android.arch.lifecycle.LiveData;
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.OnConflictStrategy;
+import android.arch.persistence.room.Query;
+import android.arch.persistence.room.Transaction;
+import android.arch.persistence.room.Update;
+
+import com.pizzachefassistant.repository.model.Warehouse;
+
+import java.util.List;
+
+import static android.arch.persistence.room.OnConflictStrategy.REPLACE;
+
+@Dao
+public abstract class WarehouseDao {
+    @Insert(onConflict = REPLACE)
+    public abstract void insert(Warehouse warehouse);
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    public abstract void insertAll(List<Warehouse> warehouses);
+
+    @Query("SELECT * FROM `Warehouse` WHERE warehouseUserID = :warehouseUserID AND warehouseIngredientID = :warehouseIngredientID")
+    public abstract LiveData<Warehouse> load(int warehouseUserID, int warehouseIngredientID);
+
+    @Query("SELECT * FROM `Warehouse`")
+    public abstract LiveData<List<Warehouse>> loadAll();
+
+    @Update
+    public abstract void updateWarehouses(Warehouse... warehouses);
+
+    @Query("DELETE FROM `Warehouse`")
+    public abstract void deleteAll();
+
+    @Transaction
+    public void deleteAndCreate(List<Warehouse> warehouses) {
+        deleteAll();
+        insertAll(warehouses);
+    }
+}
--- app/src/main/java/com/pizzachefassistant/repository/model/Warehouse.java
+++ app/src/main/java/com/pizzachefassistant/repository/model/Warehouse.java
@@ -3,19 +3,18 @@ package com.pizzachefassistant.repository.model;
 import android.arch.persistence.room.Entity;
 import android.arch.persistence.room.ForeignKey;
 
-@Entity(tableName = "warehouse_join",
+@Entity(
         primaryKeys = {"warehouseUserID","warehouseIngredientID"},
-        foreignKeys = {@ForeignKey(entity = User.class,parentColumns = "userID", childColumns = "warehouseUserID"),
-                       @ForeignKey(entity = Ingredient.class, parentColumns = "ingredientID", childColumns = "warehouseIngredientID")
+        foreignKeys = {@ForeignKey(entity = User.class, parentColumns = "id", childColumns = "warehouseUserID"),
+                       @ForeignKey(entity = Ingredient.class, parentColumns = "id", childColumns = "warehouseIngredientID")
 })
-
 public class Warehouse {
-    public final int warehouseUserID;
-    public final int warehouseIngredientID;
-    public final int amount;
-    public final int capacity;
+    public int warehouseUserID;
+    public int warehouseIngredientID;
+    public int amount;
+    public int capacity;
 
-    public Warehouse (final int warehouseIngredientID,final int warehouseUserID, int amount, int capacity){
+    public Warehouse (int warehouseIngredientID, int warehouseUserID, int amount, int capacity){
         this.warehouseIngredientID = warehouseIngredientID;
         this.warehouseUserID = warehouseUserID;
         this.amount = amount;
