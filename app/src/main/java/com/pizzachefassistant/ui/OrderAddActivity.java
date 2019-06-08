743d6172a4aa6b553e5ebf9ff5157c441a3c5ec9
Wojciech Juskowiak <juski552@gmail.com>
2019-06-08 21:41:29 +0200

    Views readjust and popup windows

--- app/src/main/java/com/pizzachefassistant/ui/OrderAddActivity.java
+++ app/src/main/java/com/pizzachefassistant/ui/OrderAddActivity.java
@@ -3,6 +3,12 @@ package com.pizzachefassistant.ui;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
 
 import com.pizzachefassistant.R;
 
@@ -16,6 +22,25 @@ public class OrderAddActivity extends AppCompatActivity {
 
         if (actionBar != null)
             actionBar.setDisplayHomeAsUpEnabled(true);
+    }
+
+    public void onButtonShowPopupWindowClick(View view) {
+
+        LayoutInflater inflater = (LayoutInflater)
+                getSystemService(LAYOUT_INFLATER_SERVICE);
+        View popupView = inflater.inflate(R.layout.add_pizza_to_order, null);
+        int width = LinearLayout.LayoutParams.WRAP_CONTENT;
+        int height = LinearLayout.LayoutParams.WRAP_CONTENT;
+        boolean focusable = true; // lets taps outside the popup also dismiss it
+        final PopupWindow popupWindow = new PopupWindow(popupView, width, height, focusable);
+        popupWindow.showAtLocation(view, Gravity.CENTER, 0, 0);
+        popupView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                popupWindow.dismiss();
+                return true;
+            }
+        });
     }
 
 }
--- app/src/main/java/com/pizzachefassistant/ui/PizzaAddActivity.java
+++ app/src/main/java/com/pizzachefassistant/ui/PizzaAddActivity.java
@@ -5,6 +5,12 @@ import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
 
 import com.pizzachefassistant.R;
 import com.pizzachefassistant.databinding.AddPizzaBinding;
@@ -29,5 +35,24 @@ public class PizzaAddActivity extends AppCompatActivity {
 
         if (actionBar != null)
             actionBar.setDisplayHomeAsUpEnabled(true);
+    }
+
+    public void onButtonShowPopupWindowClick(View view) {
+
+        LayoutInflater inflater = (LayoutInflater)
+                getSystemService(LAYOUT_INFLATER_SERVICE);
+        View popupView = inflater.inflate(R.layout.add_ingredient_to_pizza, null);
+        int width = LinearLayout.LayoutParams.WRAP_CONTENT;
+        int height = LinearLayout.LayoutParams.WRAP_CONTENT;
+        boolean focusable = true; // lets taps outside the popup also dismiss it
+        final PopupWindow popupWindow = new PopupWindow(popupView, width, height, focusable);
+        popupWindow.showAtLocation(view, Gravity.CENTER, 0, 0);
+        popupView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                popupWindow.dismiss();
+                return true;
+            }
+        });
     }
 }
