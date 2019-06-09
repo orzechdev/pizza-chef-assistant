d0dfb29855a2520ddfca0bd660869bc42eada242
Wojciech Juskowiak <juski552@gmail.com>
2019-06-09 17:48:15 +0200

    Dim on PopUp

--- app/src/main/java/com/pizzachefassistant/ui/OrderAddActivity.java
+++ app/src/main/java/com/pizzachefassistant/ui/OrderAddActivity.java
@@ -1,5 +1,7 @@
 package com.pizzachefassistant.ui;
 
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
@@ -14,6 +16,8 @@ import com.pizzachefassistant.R;
 
 public class OrderAddActivity extends AppCompatActivity {
 
+    private android.support.constraint.ConstraintLayout popupdim;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.add_order);
@@ -22,6 +26,7 @@ public class OrderAddActivity extends AppCompatActivity {
 
         if (actionBar != null)
             actionBar.setDisplayHomeAsUpEnabled(true);
+        popupdim = findViewById(R.id.add_order_popup);
     }
 
     public void onButtonShowPopupWindowClick(View view) {
@@ -29,6 +34,9 @@ public class OrderAddActivity extends AppCompatActivity {
         LayoutInflater inflater = (LayoutInflater)
                 getSystemService(LAYOUT_INFLATER_SERVICE);
         View popupView = inflater.inflate(R.layout.add_pizza_to_order, null);
+        int color = 0x80000000;
+        Drawable drawable = new ColorDrawable(color);
+        popupdim.setForeground(drawable);
         int width = LinearLayout.LayoutParams.WRAP_CONTENT;
         int height = LinearLayout.LayoutParams.WRAP_CONTENT;
         boolean focusable = true; // lets taps outside the popup also dismiss it
@@ -39,6 +47,14 @@ public class OrderAddActivity extends AppCompatActivity {
             public boolean onTouch(View v, MotionEvent event) {
                 popupWindow.dismiss();
                 return true;
+            }
+        });
+        popupWindow.setOnDismissListener(new PopupWindow.OnDismissListener() {
+            @Override
+            public void onDismiss() {
+                int color = 0x00000000;
+                Drawable drawable = new ColorDrawable(color);
+                popupdim.setForeground(drawable);
             }
         });
     }
--- app/src/main/java/com/pizzachefassistant/ui/PizzaAddActivity.java
+++ app/src/main/java/com/pizzachefassistant/ui/PizzaAddActivity.java
@@ -2,9 +2,13 @@ package com.pizzachefassistant.ui;
 
 import android.arch.lifecycle.ViewModelProviders;
 import android.databinding.DataBindingUtil;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.text.style.ForegroundColorSpan;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
@@ -20,6 +24,8 @@ public class PizzaAddActivity extends AppCompatActivity {
 
     private AddPizzaBinding binding;
     private PizzaAddViewModel viewModel;
+    private android.support.constraint.ConstraintLayout popupdim;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,16 +41,22 @@ public class PizzaAddActivity extends AppCompatActivity {
 
         if (actionBar != null)
             actionBar.setDisplayHomeAsUpEnabled(true);
+        popupdim = findViewById(R.id.add_pizza_popup);
     }
+
+
 
     public void onButtonShowPopupWindowClick(View view) {
 
         LayoutInflater inflater = (LayoutInflater)
                 getSystemService(LAYOUT_INFLATER_SERVICE);
         View popupView = inflater.inflate(R.layout.add_ingredient_to_pizza, null);
+        int color = 0x80000000;
+        Drawable drawable = new ColorDrawable(color);
+        popupdim.setForeground(drawable);
         int width = LinearLayout.LayoutParams.WRAP_CONTENT;
         int height = LinearLayout.LayoutParams.WRAP_CONTENT;
-        boolean focusable = true; // lets taps outside the popup also dismiss it
+        boolean focusable = true;
         final PopupWindow popupWindow = new PopupWindow(popupView, width, height, focusable);
         popupWindow.showAtLocation(view, Gravity.CENTER, 0, 0);
         popupView.setOnTouchListener(new View.OnTouchListener() {
@@ -52,6 +64,14 @@ public class PizzaAddActivity extends AppCompatActivity {
             public boolean onTouch(View v, MotionEvent event) {
                 popupWindow.dismiss();
                 return true;
+            }
+        });
+        popupWindow.setOnDismissListener(new PopupWindow.OnDismissListener() {
+            @Override
+            public void onDismiss() {
+                int color = 0x00000000;
+                Drawable drawable = new ColorDrawable(color);
+                popupdim.setForeground(drawable);
             }
         });
     }
